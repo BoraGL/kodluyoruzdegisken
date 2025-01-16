fb70c9831fc4cb69a1a4403e58a9592139b95b04
Bora Güneral <boraguneral@outlook.com>
2025-01-16 22:11:59 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace tip_donusumleri
+class Program2
+
+{
+    
+
+
+static void Main(string[] args){
+
+    //Implicit
+
+    byte a = 5;
+    sbyte b = 30;
+    short c = 10;
+
+    int d = a +b+c;
+
+    Console.WriteLine("d: " + d);
+
+    long h = d;
+    Console.WriteLine("h" + h);
+
+    float f = h;
+    Console.WriteLine("i" + i);
+
+    string s = "bora";
+    char f = 'B';
+    object j = e+f+d;
+    Console.WriteLine("j:" + j);
+
+    //Explicit
+
+    int x = 4;
+    byte by = x;
+
+
+
+}}
